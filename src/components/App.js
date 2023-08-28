074b67c03ba99db620ffdf35e204b2570d05a427
Rae Stanton <raestanton93@gmail.com>
2023-08-28 11:13:14 -0500

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 // create your App component here
 import React, { useState, useEffect } from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [grabDog, setGrabDog] = useState(null);
+
   useEffect(() => {
+    setIsLoading(true);
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => response.json())
-      .then((data) => setGrabDog(data.image));
+      .then((data) => {
+        setGrabDog(data.message);
+        setIsLoading(false);
+      });
   }, []);
 
   //return image data here
   return (
-  <div className="dog-images">
-    <p>Loading...</p>
-    //set up once loaded function to display the image with alt text "A random dog"
-  </div>
+    <div className="dog-images">
+      {isLoading ? <p>Loading...</p> : null}
+      {grabDog && <img src={grabDog} alt="A Random Dog" />}
+    </div>
   )
 }
