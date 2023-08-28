de21be3181c1adb988cc8aaf3afd3e068b81ee9b
Rae Stanton <raestanton93@gmail.com>
2023-08-28 10:32:05 -0400

    wip fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+import { ReactDOM } from "react-dom";
+
+export default function App() {
+  const [grabDog, setGrabDog] = useState(null);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setGrabDog(data.image));
+  }, []);
+
+  //return image data here
+  return (
+  <div className="dog-images">
+    <p>Loading...</p>
+    //set up once loaded function to display the image with alt text "A random dog"
+  </div>
+  )
+}
